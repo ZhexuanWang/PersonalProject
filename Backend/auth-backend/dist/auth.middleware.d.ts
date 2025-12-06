3203a59d35f37b25035a2904a87139ed43907d9f
wangergou <w2x18913251576@163.com>
2025-12-06 13:50:29 +0300

    Modified:vercel.json

--- Backend/auth-backend/dist/auth.middleware.d.ts
+++
@@ -1,3 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-export declare function requireAuth(req: Request, res: Response, next: NextFunction): void | Response<any, Record<string, any>>;
-//# sourceMappingURL=auth.middleware.d.ts.map

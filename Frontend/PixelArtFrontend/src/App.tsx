642c454a6e46fdd77b6dd120f90470b974664efc
wangergou <w2x18913251576@163.com>
2025-12-16 18:40:38 +0300

    test not to use /profile

--- Frontend/PixelArtFrontend/src/App.tsx
+++ Frontend/PixelArtFrontend/src/App.tsx
@@ -5,15 +5,16 @@ import Ribbon from "./components/Ribbon/Ribbon.tsx";
 import AuthArea from "./components/AuthArea/AuthArea.tsx";
 import {Route, Routes} from "react-router-dom";
 import Profile from "./components/Profile/Profile.tsx";
+import {useUIContext} from "./contexts/UIContext/UIContext.tsx";
 
 function App() {
-
+    const {isLoggedIn} = useUIContext();
     return (
         <>
             <Routes>
                 <Route path="/" element={<></>} />
-                <Route path="/profile" element={<Profile />}></Route>
             </Routes>
+            {isLoggedIn&&<Profile />}
             <Ribbon/>
             <AuthArea/>
             <InputArea/>

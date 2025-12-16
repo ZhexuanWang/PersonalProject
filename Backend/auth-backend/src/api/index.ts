57caca916618f1c3fb57fbc17f6541634c9a435b
wangergou <w2x18913251576@163.com>
2025-12-16 18:05:32 +0300

    change the regular expression to match /profile

--- Backend/auth-backend/src/api/index.ts
+++ Backend/auth-backend/src/api/index.ts
@@ -27,7 +27,7 @@ const corsOptions = {
         ];
 
         // 正则表达式匹配所有 Vercel 预览域名
-        const vercelRegex = /https:\/\/personal-project-frontend-.+-zhexuanwangs-projects\.vercel\.app/;
+        const vercelRegex = /^https:\/\/personal-project-frontend-[^.]+-zhexuanwangs-projects\.vercel\.app$/;
 
         if (!origin || allowedOrigins.includes(origin) || vercelRegex.test(origin)) {
             callback(null, true);
